18169dc2839b783c40005e080b1e7103fd088dbe
Joan Costes <contact@welcometomasomenos.com>
2026-04-20 21:33:36 +0200

    Fix Safari video + add location to description

--- src/components/GuestBooking.jsx
+++ src/components/GuestBooking.jsx
@@ -375,7 +375,6 @@ export default function GuestBooking() {
                 style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', border: 'none' }}
                 allow="autoplay; fullscreen; picture-in-picture"
                 allowFullScreen
-                referrerPolicy="no-referrer"
                 loading="lazy"
                 title="Chateaumoulin"
               />
